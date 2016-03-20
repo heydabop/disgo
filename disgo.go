84dfb015396a76e72324ff3eefdb57fc01f128ee
heydabop <heydabop@gmail.com>
2016-03-20 13:24:17 -0500

    add roll and help

--- disgo.go
+++ disgo.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"math/rand"
 	"regexp"
 	"strings"
+	"strconv"
 	"time"
 )
 
@@ -16,13 +18,11 @@ func twitch(args []string) (string, error) {
 	cmd := exec.Command("/home/ross/markov/1-markov.out", "1")
 	logs, err := os.Open("/home/ross/markov/" + args[0] + "_nolink")
 	if err != nil {
-		fmt.Println(err)
 		return "", err
 	}
 	cmd.Stdin = logs
 	out, err := cmd.Output()
 	if err != nil {
-		fmt.Println(err)
 		return "", err
 	}
 	return strings.TrimSpace(string(out)), nil
@@ -40,6 +40,24 @@ func forsen(args []string) (string, error) {
 	return twitch([]string{"forsenlol"})
 }
 
+func roll(args []string) (string, error) {
+	var max int
+	if (len(args) < 1) {
+		max = 6
+	} else {
+		var err error
+		max, err = strconv.Atoi(args[0])
+		if err != nil || max < 0 {
+			return "", err
+		}
+	}
+	return strconv.Itoa(rand.Intn(max) + 1), nil
+}
+
+func help(args []string) (string, error) {
+	return "twitch [streamer], soda, lirik, forsen, roll [sides (optional)]", nil
+}
+
 func makeMessageCreate() func(*discordgo.Session, *discordgo.MessageCreate) {
 	const myUserID = "160807650345353226"
 	regexes := []*regexp.Regexp{regexp.MustCompile(`^<@` + myUserID + `>\s+(.+)`), regexp.MustCompile(`^\/(.+)`)}
@@ -48,6 +66,8 @@ func makeMessageCreate() func(*discordgo.Session, *discordgo.MessageCreate) {
 		"soda": command(soda),
 		"lirik": command(lirik),
 		"forsen": command(forsen),
+		"roll": command(roll),
+		"help": command(help),
 	}
 
 	return func (s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -69,6 +89,7 @@ func makeMessageCreate() func(*discordgo.Session, *discordgo.MessageCreate) {
 				if cmd, valid := funcMap[strings.ToLower(command[0])]; valid {
 					reply, err := cmd(command[1:])
 					if err != nil {
+						fmt.Println("ERROR: " + err.Error())
 						return
 					}
 					s.ChannelMessageSend(m.ChannelID, reply)
