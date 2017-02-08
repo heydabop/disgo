650d05a36e699b0e8cd02f95bf78292235b7ac11
heydabop <heydabop@gmail.com>
2017-02-08 01:27:04 -0600

    remove unused scripts

--- gen_custom_log.sh
+++
@@ -1,5 +0,0 @@
-psql -td disgo -c "SELECT content FROM message WHERE chan_id = '$1' AND author_id = '$2' AND content != '';" > ~/markov/$2_custom
-sed -i -r 's,<@.*?> ,,gm' ~/markov/$2_custom
-sed -i -r '/^\/.*/d' ~/markov/$2_custom
-sed -i -r 's,<@.*?>,,gm' ~/markov/$2_custom
-sed -i -r '/^$/d' ~/markov/$2_custom
--- gen_custom_log_by_chan.sh
+++
@@ -1,5 +0,0 @@
-psql -td disgo -c "SELECT content FROM message WHERE chan_id = '$1' AND content != '';" > ~/markov/chan_$1_custom
-sed -i -r 's,<@.*?> ,,gm' ~/markov/chan_$1_custom
-sed -i -r '/^\/.*/d' ~/markov/chan_$1_custom
-sed -i -r 's,<@.*?>,,gm' ~/markov/chan_$1_custom
-sed -i -r '/^$/d' ~/markov/chan_$1_custom
